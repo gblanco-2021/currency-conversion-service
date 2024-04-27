ae68ba869e1beb15c06adb094c30160d8a54d1a1
Gustavo <devgblanco@gmail.com>
2024-04-27 14:52:03 -0500

    update pom

--- src/main/java/com/gblanco/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/gblanco/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,6 +1,10 @@
 package com.gblanco.microservices.currencyconversionservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +15,24 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
+@Slf4j
 @RestController
 public class CurrencyConversionController {
 
     @Autowired
     private CurrencyExchangeProxy proxy;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -23,7 +40,7 @@ public class CurrencyConversionController {
             @PathVariable String to,
             @PathVariable BigDecimal quantity
     ) {
-        RestTemplate restTemplate = new RestTemplate();
+        log.info("calling data defautl por 8000");
         Map<String, String> uriVariables = new HashMap<>();
         uriVariables.put("from", from);
         uriVariables.put("to", to);
